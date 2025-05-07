b82843e589fab839ad7ba9117acd801a8e562e84
Your Name <you@example.com>
2025-05-07 09:33:32 -0400

    wuju

--- tarea2.c
+++ tarea2.c
@@ -22,7 +22,7 @@ typedef struct {
 void mostrarMenuPrincipal() { //LISTO
 
     puts("========================================") ;
-    puts("     Base de Datos de Canciones") ;
+    puts("              SPOTIFIND") ;
     puts("========================================");
     puts("1) Cargar Canciones") ;
     puts("2) Buscar por Genero") ;
@@ -50,7 +50,7 @@ void leer_canciones(Map* mapID, Map* mapArtista, Map* mapGenero, long cancionesM
     // Lee cada línea del archivo CSV hasta el final
     while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
       
-        if (k > cancionesMax) break; //carga primeras n canciones
+        if (k >= cancionesMax) break; //carga primeras n canciones
         Cancion *cancion = malloc(sizeof(Cancion)) ;
         if (cancion == NULL) exit(EXIT_FAILURE) ;
         
@@ -64,33 +64,39 @@ void leer_canciones(Map* mapID, Map* mapArtista, Map* mapGenero, long cancionesM
         map_insert(mapID, cancion->ID, cancion) ; // Insertar en el mapa por ID
 
         for (char *artista = list_first(cancion->artista); artista != NULL; artista = list_next(cancion->artista)) {
-            List *listaArtista = map_search(mapArtista, artista) ;
-            if (listaArtista == NULL) {
-                listaArtista = list_create() ;
+            MapPair *artistaPar = map_search(mapArtista, artista) ;
+            if (artistaPar == NULL) {
+                List *listaArtista = list_create() ;
+                list_pushBack(listaArtista, cancion) ;
                 map_insert(mapArtista, artista, listaArtista) ;
             }
-            list_pushBack(listaArtista, cancion) ;
+            else {
+                List *listaArtista = (List*)    artistaPar->value ;
+                list_pushBack(listaArtista, cancion) ;
+            }
         }
         
-        List *listaGenero = map_search(mapGenero, cancion->genero);
-        if (listaGenero == NULL) {
-            listaGenero = list_create();
+        MapPair *generoPar = map_search(mapGenero, cancion->genero) ;
+        List *listaGenero = generoPar ? (List*)generoPar->value : NULL ;
+        if (generoPar == NULL) {
+            List *listaGenero = list_create();
+            list_pushBack(listaGenero, cancion) ;
             map_insert(mapGenero, cancion->genero, listaGenero);
         }
-        list_pushBack(listaGenero, cancion);
-
-        
-
+        else {
+            List *listaGenero = (List*)generoPar->value ;
+            list_pushBack(listaGenero, cancion);
+        }
         k++;
     }
     fclose(archivo); 
 }
 
-int is_equal_str(const void *key1, const void *key2) { 
+int is_equal_str(void *key1, void *key2) { 
     return strcmp((char *)key1, (char *)key2) == 0; 
 }
 
-int is_equal_int(const void *key1, const void *key2) {
+int is_equal_int(void *key1, void *key2) {
     return *(int *)key1 == *(int *)key2; 
 }
 
@@ -103,6 +109,35 @@ void busquedaGenero(Map *cancionesGenero) {
 void busquedaArtista(Map *cancionesArtista) {
     /* La usuaria ingresa el nombre de un artista y la aplicación muestra todas las canciones 
     que coincidan con la información de cada una.*/
+    puts("=========================================") ;
+    puts("         BUSQUEDA POR ARTISTA") ; 
+    puts("=========================================") ;
+    printf("\nIngrese el nombre del artista: \n") ;
+
+    char artista[100] ;
+    fgets (artista, sizeof(artista), stdin) ;
+    artista[strcspn(artista, "\n")] = '\0' ; 
+    
+    if (cancionesArtista == NULL) {
+        printf("No hay canciones en registro\n") ;
+        return ;
+    } 
+    
+    MapPair *par = map_search(cancionesArtista, artista) ;
+    if (par == NULL) {
+        printf("No se encontraron canciones de %s\n", artista) ;
+        return ;
+    } 
+    else {
+        List *canciones = (List *)par->value ;
+        Cancion *cancion = list_first(canciones) ;
+        while (cancion != NULL) {
+            printf("ID: %s | Titulo: %s | Album: %s | Genero: %s | Tempo: %d BPM\n", 
+                cancion->ID, cancion->titulo, cancion->album, cancion->genero, cancion->tempo) ;
+            cancion = list_next(canciones) ;
+        }
+    }
+
 }
 
 //void busquedaTempo( ){
@@ -135,13 +170,13 @@ int main() {
                 leer_canciones(cancionesID, cancionesArtista, cancionesGenero, cancionesCargadas) ;
                 break;
             case 2:
-                busquedaGenero(cancionesGenero) ;
+                //busquedaGenero(cancionesGenero) ;
                 break ;
             case 3:
                 busquedaArtista(cancionesArtista) ;
                 break ;
             case 4:
-                busquedaTempo() ;
+                //busquedaTempo() ;
                 break ;
             case 5:
                 //pendejada
